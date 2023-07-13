047517ab4add525ac6a31e98b20e4c1e83942879
Surbhi LNU <slnu@chegg.com>
2023-07-13 14:51:42 +0530

    user authentication completed

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const { CLIENT_URL } = require("./constants");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const cors = require("cors");
+
+//import passport middleware
+require("./middlewares/passport-middleware");
+
+//initialize middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(passport.initialize());
+
+//import routes
+const authRoutes = require("./routes/auth");
+
+//initilaize routes
+app.use("/api", authRoutes);
+
+//app start
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`The app is running at http://localhost:${PORT}`);
+});
